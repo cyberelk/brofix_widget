605d8546ce9a0f2ee2d693d99b5c66c9042b336a
Jari-Hermann Ernst <webdev@jhernst.de>
2025-01-24 13:11:31 +0100

    [TASK] Consideration of the new check status and correction of the output in the widget

--- Classes/Widgets/BrofixWidget.php
+++ Classes/Widgets/BrofixWidget.php
@@ -50,8 +50,41 @@ class BrofixWidget implements WidgetInterface
             ->executeQuery()
             ->fetchAllAssociative();
 
+        // Define meaningful constants for the status values
+        define('STATUS_FAULTY', '1');
+        define('STATUS_OK', '2');
+        define('STATUS_NOT_TESTABLE', '3');
+        define('STATUS_EXCLUDED', '4');
+
+        // Initialize counts array with predefined keys
+        $countArray = [
+            'faulty' => 0,
+            'ok' => 0,
+            'not_testable' => 0,
+            'excluded' => 0,
+            'unknown' => 0,
+        ];
+
+        // Mapping of status codes to array keys
+        $statusMap = [
+            STATUS_FAULTY => 'faulty',
+            STATUS_OK => 'ok',
+            STATUS_NOT_TESTABLE => 'not_testable',
+            STATUS_EXCLUDED => 'excluded'
+        ];
+
+        foreach ($brokenLinks as $brokenLink) {
+            if (isset($statusMap[$brokenLink['check_status']])) {
+                $key = $statusMap[$brokenLink['check_status']];
+                $countArray[$key]++;
+            } else {
+                $countArray['unknown']++;
+            }
+        }
+
         $this->view->assignMultiple([
-            'count' => count($brokenLinks),
+            'countOverall' => $countArray['faulty'] + $countArray['not_testable'],
+            'countArray' => $countArray,
             'beUserAccess' => $beUserAccess,
             'configuration' => $this->configuration,
         ]);
