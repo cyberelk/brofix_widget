658478009a25a4311eb25fd6af24bd2b64084174
Jari-Hermann Ernst <jari-hermann.ernst@t-online.de>
2025-04-28 10:07:31 +0200

    TYPO3 v12 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,12 +8,12 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'webdev@jhernst.de',
     'state' => 'experimental',
     'clearCacheOnLoad' => 0,
-    'version' => '0.0.3',
+    'version' => '0.0.4',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99',
+            'typo3' => '13.4.0-13.4.99',
             'brofix' => '6.3.0-6.3.99',
-            'cms-dashboard' => '12.4.0-12.4.99',
+            'cms-dashboard' => '13.4.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
